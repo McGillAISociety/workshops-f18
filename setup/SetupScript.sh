8f7c6af120fb6cdd84139c740a94a16341c96002
Isaac Chan <isaac.chan@mail.mcgill.ca>
2018-09-10 20:34:59 -0400

    Added setup scripts for workshop 1

---
+++ setup/SetupScript.sh
@@ -0,0 +1,26 @@
+#
+#  Original solution via StackOverflow:
+#    http://stackoverflow.com/questions/35802939/install-only-available-packages-using-conda-install-yes-file-requirements-t
+#
+
+#
+#  Install via `conda` directly.
+#  This will fail to install all
+#  dependencies. If one fails,
+#  all dependencies will fail to install.
+#
+
+echo Input desired virtual environment name
+read envName
+export PATH=~/anaconda3/bin:$PATH
+conda create -n $envName -y python=3.6 jupyter
+source activate $envName
+#pip install -r pip_requirements.txt
+conda install --yes --file requirements.txt
+
+#
+#  To go around issue above, one can
+#  iterate over all lines in the
+#  requirements.txt file.
+#
+while read requirement; do conda install --yes $requirement; done < conda_requirements.txt
